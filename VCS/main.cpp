9c90f99597c4140ebe6944f30a08dea330d9fe9d
Roaa-26 <roamagdy26@gmail.com>
2023-10-30 12:14:39 +0200

    "Adding getAverage Function"

--- VCS/main.cpp
+++ VCS/main.cpp
@@ -9,13 +9,27 @@ int getSum(std::vector<int> elements){
     }
     return sum;
 }
+
+float getAverage(std::vector<int> elements){
+    float average=0;
+    for(int i=0 ; i<elements.size(); i++){
+        average+=elements[i];
+    }
+
+    average=average/elements.size();
+
+    return average;
+}
+
 int main(){
 
     std::vector<int> intValues{1,2,3,4,5,6,7,8,9,10};
 
     int vectorSummation = getSum(intValues);
+    float vectorAverage = getAverage(intValues);
 
     std::cout<<"Array summation = "<<vectorSummation<<std::endl;
+    std::cout<<"Average of array elements= "<<vectorAverage<<std::endl;
 
     return 0;
 }
