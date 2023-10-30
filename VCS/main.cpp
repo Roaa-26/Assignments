28cd269666d766311586e61cbc6177de3f183157
Roaa-26 <roamagdy26@gmail.com>
2023-10-30 12:29:24 +0200

    "Adding getMin Function"

--- VCS/main.cpp
+++ VCS/main.cpp
@@ -9,13 +9,32 @@ int getSum(std::vector<int> elements){
     }
     return sum;
 }
+
+int getMin(std::vector<int> elements){
+    
+    int min;
+
+    for(int i=0 ; i<elements.size(); i++){
+        if(i==0){
+            min = elements[0];
+        }
+        else if(min > elements[i]){
+            min = elements[i];
+        }
+    }
+
+    return min;
+}
+
 int main(){
 
     std::vector<int> intValues{1,2,3,4,5,6,7,8,9,10};
 
     int vectorSummation = getSum(intValues);
+    int minValue = getMin(intValues);
 
     std::cout<<"Array summation = "<<vectorSummation<<std::endl;
+    std::cout<<"Minimum value in array = "<<minValue<<std::endl;
 
     return 0;
 }
